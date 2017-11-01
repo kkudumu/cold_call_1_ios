c54f1eaa09e7117c78a02265ab79a026c15d9049
Kio <kkudumu@gmail.com>
2017-11-01 15:39:36 -0700

    cold call 2 complete

--- Cold_Call_iOS/ViewController.swift
+++ Cold_Call_iOS/ViewController.swift
@@ -10,23 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
-   
+    @IBOutlet weak var numLabel: UILabel!
+    
     let nameBank = [
         "Jimmy", "Johnny", "Jonas", "John", "Jeremy", "Julia", "Jill"
     ]
-    
+
     @IBAction func callButton(_ sender: UIButton) {
-        let cold_call = Int(arc4random_uniform(5) + 1)
+            let cold_call = Int(arc4random_uniform(5) + 1)
         nameLabel.text = nameBank[cold_call]
-    }
+        numColor()
+        
+        }
     
+    func numColor() {
+        let cold_call = Int(arc4random_uniform(5) + 1)
+        numLabel.text = String(cold_call)
+        if String(cold_call) == "1" || String(cold_call) == "2" {
+            numLabel.textColor = UIColor.red
+            
+        }
+        else if String(cold_call) == "3" || String(cold_call) == "4" {
+            numLabel.textColor = UIColor.orange
+        }
+        else if String(cold_call) == "5"{
+            numLabel.textColor = UIColor.green
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-    func updateUI() {
-    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
