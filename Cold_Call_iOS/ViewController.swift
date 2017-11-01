ee8dee861617ab2f69f7f6c993a2f33ec1c966d8
Kio <kkudumu@gmail.com>
2017-11-01 13:58:21 -0700

    cold call 1 complete

--- Cold_Call_iOS/ViewController.swift
+++ Cold_Call_iOS/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+   
+    let nameBank = [
+        "Jimmy", "Johnny", "Jonas", "John", "Jeremy", "Julia", "Jill"
+    ]
+    
+    @IBAction func callButton(_ sender: UIButton) {
+        let cold_call = Int(arc4random_uniform(5) + 1)
+        nameLabel.text = nameBank[cold_call]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func updateUI() {
     }
 
     override func didReceiveMemoryWarning() {
